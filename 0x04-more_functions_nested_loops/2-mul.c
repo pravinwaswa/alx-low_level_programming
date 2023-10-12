685e3138c7032276de82eb63fa08122179647561
pravin <pravinwaswa@gmail.com>
2023-10-12 07:43:26 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul -  a functon that checks for checks for a digit (0 through 9).
+ * @a: a - A Variable
+ * @b: b - A variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
